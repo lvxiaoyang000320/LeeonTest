c47bef9311f9e700997713657241eb92eafc46a8
wangjun <ybt@ybtdeiMac.local>
2018-01-11 09:38:29 +0800

    1

---
+++ LLLLL/LLLLL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLLLL
+//
+//  Created by ybt on 2018/1/11.
+//  Copyright © 2018年 logDealTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LLLLL/LLLLL/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LLLLL
+//
+//  Created by ybt on 2018/1/11.
+//  Copyright © 2018年 logDealTeam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
