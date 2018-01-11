6cd54e28e68eb2a88d3eb2ef6e5a4440d93e922d
wangjun <ybt@ybtdeiMac.local>
2018-01-11 09:44:58 +0800

    4

--- LLLLL/LLLLL/ViewController.m
+++ LLLLL/LLLLL/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
 //    self.view.bounds
+    NSLog(@"%@", self.description);
+    NSLog(@"%@", self.view.description);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
